8e734c2b0e463a222c1610ae2de25e14912268ca
tombia79 <74193316+tombia79@users.noreply.github.com>
2025-10-02 12:36:13 -0500

    ping.js

---
+++ netlify/functions/ping.js
@@ -0,0 +1,7 @@
+export const handler = async () => {
+  return {
+    statusCode: 200,
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ ok: true, msg: 'ping' }),
+  };
+};
