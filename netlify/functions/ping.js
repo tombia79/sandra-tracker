84502954ff1c25d33dbc185fb82c0c8fc8576965
tombia79 <74193316+tombia79@users.noreply.github.com>
2025-10-02 12:44:28 -0500

    Delete netlify/functions/ping.js

--- netlify/functions/ping.js
+++
@@ -1,7 +0,0 @@
-export const handler = async () => {
-  return {
-    statusCode: 200,
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ ok: true, msg: 'ping' }),
-  };
-};
